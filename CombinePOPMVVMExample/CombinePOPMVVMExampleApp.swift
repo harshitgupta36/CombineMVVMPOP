acd25471d68a4557dd193af4db9e9ae59c1944f3
HarshitGupta4 <vighnablitz@gmail.com>
2024-08-11 09:18:09 +0530

    Initial Commit

---
+++ CombinePOPMVVMExample/CombinePOPMVVMExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombinePOPMVVMExampleApp.swift
+//  CombinePOPMVVMExample
+//
+//  Created by Harshit Vaish on 11/08/24.
+//
+
+import SwiftUI
+
+@main
+struct CombinePOPMVVMExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
